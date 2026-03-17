2f5b761f458f8e72c6dd1bdd9c8c732575f505b6
Zhouwen-CN <597879949@qq.com>
2026-03-17 08:38:53 +0800

    feat: 添加 EtlField 字段定义类

---
+++ flink-etl-core/src/main/java/com/etl/core/schema/EtlField.java
@@ -0,0 +1,29 @@
+package com.etl.core.schema;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * ETL 字段定义
+ * 包含字段名和类型
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EtlField implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 字段名
+     */
+    private String name;
+
+    /**
+     * 字段类型
+     */
+    private EtlFieldType type;
+}
