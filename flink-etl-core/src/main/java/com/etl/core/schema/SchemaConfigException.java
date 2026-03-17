5b043f515f1214e88bd80f1e5cdc7547eb2327ce
Zhouwen-CN <597879949@qq.com>
2026-03-17 08:41:41 +0800

    feat: 添加 SchemaConfigException 异常类

---
+++ flink-etl-core/src/main/java/com/etl/core/schema/SchemaConfigException.java
@@ -0,0 +1,12 @@
+package com.etl.core.schema;
+
+/**
+ * Schema 配置异常
+ * 当 Schema 配置格式错误时抛出
+ */
+public class SchemaConfigException extends RuntimeException {
+
+    public SchemaConfigException(String message) {
+        super("Schema 配置错误: " + message);
+    }
+}
