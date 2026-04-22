f066b8492d391b9e204d6e3519722ead13990b89
Zhouwen-CN <597879949@qq.com>
2026-04-22 10:50:06 +0800

    feat: 添加 MySQL CDC 启动模式枚举

---
+++ flink-etl-connector/connector-cdc/src/main/java/com/etl/connector/cdc/mysql/StartupMode.java
@@ -0,0 +1,26 @@
+package com.etl.connector.cdc.mysql;
+
+/**
+ * MySQL CDC 启动模式
+ */
+public enum StartupMode {
+    /**
+     * 从最早可用位置开始（读取历史变更）
+     */
+    EARLIEST,
+
+    /**
+     * 从最新位置开始（只捕获新变更）
+     */
+    LATEST,
+
+    /**
+     * 从指定时间戳开始
+     */
+    TIMESTAMP,
+
+    /**
+     * 先读取全量快照，再捕获增量变更
+     */
+    SNAPSHOT_FIRST
+}
